450dee073a8709126dde73eccbd17c8e23491ce3
charlessilva1983 <charles.silva1983@gmail.com>
2018-09-04 17:57:21 -0300

    The first commit with the python script solution2P3Q.py that defines the implementation of the function solution2P3Q.

---
+++ solution2P3Q.py
@@ -0,0 +1,28 @@
+# A Python function that, given a non-negative integer N, 
+# returns the value A[N] in a generated set S that consist of 
+# numbers of the form 2^P*3^Q, where P and Q are non-negative integers.
+
+# N is the number of elements generated
+def solution2P3Q(N):
+    list2p3q = []
+    p = 0
+    while p < N:
+        v2p = 2 ** p
+        q = 0
+        while q < N:
+            v3q = 3 ** q
+            v2p3q = v2p * v3q
+            # store the calculated value
+            list2p3q.append(v2p3q)
+<<<<<<< HEAD
+            q+= 1
+=======
+            q += 1
+>>>>>>> c092e86195a114a1790ccd453cb7f245ed01b593
+        p+= 1
+    # sorting the values
+    list2p3q.sort()
+    # return the element in N'th index
+    return list2p3q[N]
+
+print(solution2P3Q(8))
